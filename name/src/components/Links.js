9d8626830872e806d89bc103d96264cb39643ef5
Florakimm2 <florakimm2@gmail.com>
2024-09-28 20:53:54 +0900

    .

---
+++ name/src/components/Links.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Links.css';
+
+const Links = () => {
+    return (
+        <>
+            <main className='linkmain'>
+                <div>content</div>
+            </main>
+        </>
+    );
+};
+
+export default Links;
